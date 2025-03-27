52885ff683f4309ad41e6d8a034ca5231ac80f9a
sdshook <32558333+sdshook@users.noreply.github.com>
2025-03-26 19:37:44 -0700

    Update AzADSR.ps1

--- AzADSR.ps1
+++ AzADSR.ps1
@@ -1,5 +1,5 @@
 # Microsoft Azure Active Directory Signins Report
-# Created by Shane Shook (c)2024
+# Updated by Shane Shook (c)2025
 # Note: adjust username and days as required
 
 # Requires PSv7 and DotNetv8
@@ -9,29 +9,23 @@
 ## Install-Module -Name Microsoft.Graph.Beta 
 
 Function Load-Powershell_7{
-
     function New-OutOfProcRunspace {
         param($ProcessId)
 
         $connectionInfo = New-Object -TypeName System.Management.Automation.Runspaces.NamedPipeConnectionInfo -ArgumentList @($ProcessId)
-
         $TypeTable = [System.Management.Automation.Runspaces.TypeTable]::LoadDefaultTypeFiles()
-
-        #$Runspace = [System.Management.Automation.Runspaces.RunspaceFactory]::CreateOutOfProcessRunspace($connectionInfo,$Host,$TypeTable)
         $Runspace = [System.Management.Automation.Runspaces.RunspaceFactory]::CreateRunspace($connectionInfo,$Host,$TypeTable)
-
         $Runspace.Open()
         $Runspace
     }
 
-    $Process = Start-Process PWSH -ArgumentList @("-NoExit") -PassThru -WindowStyle Hidden
-
-    $Runspace = New-OutOfProcRunspace -ProcessId $Process.Id
-
-    $Host.PushRunspace($Runspace)
+    if ($PSVersionTable.PSVersion.Major -lt 7) {
+        $Process = Start-Process PWSH -ArgumentList @("-NoExit") -PassThru -WindowStyle Hidden
+        $Runspace = New-OutOfProcRunspace -ProcessId $Process.Id
+        $Host.PushRunspace($Runspace)
+    }
 }
 Load-Powershell_7
-
 
 ###### Edit this section #####
 # Define the user
@@ -43,40 +37,47 @@ $days = 360
 # Calculate start and end dates
 $startDate = (Get-Date).AddDays(-$days)
 $endDate = Get-Date
+$outputPath = ".\output"
+New-Item -ItemType Directory -Path $outputPath -Force | Out-Null
 ###### Edit this section #####
 
+# Validate required modules
+$requiredModules = @("Microsoft.Graph", "Microsoft.Graph.Beta.Reports", "Microsoft.Graph.Authentication")
+foreach ($mod in $requiredModules) {
+    if (-not (Get-Module -ListAvailable -Name $mod)) {
+        Write-Warning "$mod is not installed. Please install it before running this script."
+    }
+}
 
 # Get User Signin Activities from Azure AD via Microsoft Graph API (will prompt for web AuthN)
-# Important Note: AzAdSigninLogs is deprecated, and Get-MgAuditLogSignIn is missing data
-# Must use Get-MgBetaAuditLogSignIn...
 Import-Module Microsoft.Graph.Authentication
 Import-Module Microsoft.Graph.Beta.Reports
 Connect-MgGraph -Scopes AuditLog.Read.All -ContextScope Process -NoWelcome 
 
-# Select Formatted Events for Review
-$azureADAuthHistory = Get-MgBetaAuditLogSignIn -All -Filter "userPrincipalName eq '$username'" | where-object {$_.createddatetime -ge $startDate}
+# Collect sign-in logs once
+$azureADAuthHistory = Get-MgBetaAuditLogSignIn -All -Filter "userPrincipalName eq '$username'" | Where-Object { $_.CreatedDateTime -ge $startDate }
+
+# Export formatted events for review
 $results = foreach ($signIn in $azureADAuthHistory) {
-	[PSCustomObject]@{
-		UserPrincipalName = $signIn.UserPrincipalName
-		UserDisplayName = $signIn.UserDisplayName
-		cDate = $signIn.CreatedDateTime
-		AppName = $signIn.AppDisplayName
-		AppUsed = $signIn.ClientAppUsed
-		Resource = $signIn.ResourceDisplayName
-		IPAddress = $signIn.IpAddress
-		City = $signIn.Location.City
-		LocST = $signIn.Location.State
-		Country = $signIn.Location.CountryOrRegion
-		DeviceId = $signIn.DeviceDetail.DeviceId
-		DeviceDetail = $signIn.DeviceDetail.DisplayName
-		DeviceOS = $signIn.DeviceDetail.OperatingSystem
-		DeviceBrowser = $signIn.DeviceDetail.Browser
-        UserAgent = $signIn.UserAgent
-	}
+    [PSCustomObject]@{
+        UserPrincipalName = $signIn.UserPrincipalName
+        UserDisplayName   = $signIn.UserDisplayName
+        cDate             = $signIn.CreatedDateTime
+        AppName           = $signIn.AppDisplayName
+        AppUsed           = $signIn.ClientAppUsed
+        Resource          = $signIn.ResourceDisplayName
+        IPAddress         = $signIn.IpAddress
+        City              = $signIn.Location.City
+        LocST             = $signIn.Location.State
+        Country           = $signIn.Location.CountryOrRegion
+        DeviceId          = $signIn.DeviceDetail.DeviceId
+        DeviceDetail      = $signIn.DeviceDetail.DisplayName
+        DeviceOS          = $signIn.DeviceDetail.OperatingSystem
+        DeviceBrowser     = $signIn.DeviceDetail.Browser
+        UserAgent         = $signIn.UserAgent
+    }
 }
-$results | Export-Csv -Path .\UALDF.csv -NoTypeInformation
+$results | Export-Csv -Path "$outputPath\UALDF.csv" -NoTypeInformation -Encoding UTF8
 
-# Raw Events for System of Record Maintenance
-$azureADAuthHistoryRaw = Get-MgBetaAuditLogSignIn -All -Filter "userPrincipalName eq '$username'" | convertto-json -depth 10
-$azureADAuthHistoryRaw | Out-File .\UALDR.json
-
+# Export raw data for system-of-record
+$azureADAuthHistory | ConvertTo-Json -Depth 10 | Out-File "$outputPath\UALDR.json" -Encoding UTF8
